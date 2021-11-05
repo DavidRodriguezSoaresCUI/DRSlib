127f811a5ce94aab54dd819bacf5334ac269ab57
DRS <david.rodriguez.1@etu.unige.ch>
2021-11-05 16:37:59 +0100

    v0.1dev2: added some functions to path_tools

--- src/DRSlib/path_tools.py
+++ src/DRSlib/path_tools.py
@@ -19,8 +19,11 @@ See elements' docstrings for further explanations.
 
 from typing import Union, Iterable, List
 from pathlib import Path
+import re
 import logging
 log = logging.getLogger( __file__ )
+
+MAKE_FS_SAFE_PATTERN = re.compile( pattern=r'[\\/*?:"<>|]' )
 
 
 class FileCollector:
@@ -88,4 +91,97 @@ def file_collector( root: Path, pattern: Union[str,Iterable[str]] = '**/*.*' ) -
         raise ValueError(f"FileCollector: 'pattern' ({pattern}) must be an Iterable or a string, but is a {type(pattern)}")
 
     return files
+
+
+def make_FS_safe( s: str ) -> str:
+    ''' File Systems don't accept all characters on file/directory names.
+    Return s with illegal characters stripped
+    Note: OS/FS agnostic, applies a simple filter on characters: backslash, /, *, ?, :, ", <, >, |
+    '''
+    return re.sub(
+        pattern=MAKE_FS_SAFE_PATTERN,
+        repl="",
+        string=s
+    )
+
+def find_available_path( root: Path, base_name, file: bool ) -> Path:
+    ''' Returns a path to a file/directory that DOESN'T already exist.
+    The file/dir the user wishes to make a path for is referred as X.
+
+    `root`: where X must be created. Can be a list of path parts
+    `base_name`: the base name for X. May be completed with '(index)' if name already exists.
+    `file`: True if X is a file, False if it is a directory
+    '''
+    # Helper function: makes suffixes for already existing files/directories
+    def suffixes():
+        yield ''
+        idx=0
+        while True:
+            idx+=1
+            yield f" ({idx})"
+    
+    # Iterate over candidate paths until an unused one is found
+    safe_base_name = make_FS_safe( base_name )
+    for suffix in suffixes():
+        _object = root / ( safe_base_name + suffix )
+        if file:
+            if not _object.is_file():
+                return _object
+        elif not _object.is_dir():
+            return _object
+
+
+def make_valid_path( 
+    root: Union[Path, List],
+    base_name: str,
+    file: bool = True,
+    create: bool = False ) -> Path:
+    ''' Returns a path to a file/directory that DOESN'T already exist.
+    The file/dir the user wishes to make a path for is referred as X.
+
+    `root`: where X must be created. Can be a list of path parts
+    `base_name`: the base name for X. May be completed with '(index)' if name already exists.
+    `file`: True if X is a file, False if it is a directory
+    `create`: True instantiates X (empty file or dir), False doesn't
+
+    Build upon `find_available_path`, adding:
+    - root path construction
+    - root mkdir
+    - ability to initialize returned file/dir
+    '''
+
+    # make root path
+    if isinstance(root, List):
+        if isinstance(root[0], str):
+            _root = Path(make_FS_safe(root[0]))
+        elif isinstance(root[0], Path):
+            _root = root[0]
+        else:
+            raise TypeError(f"root[0]={root[0]} is of unexpected type {type(root[0])}, not str or Path !")
+        
+        for path_part in root[1:]:
+            assert isinstance(path_part, str), f"path part in root '{path_part}' is of unexpected type {type(path_part)}, not str !"
+            safe_path_part = make_FS_safe(path_part)
+            assert safe_path_part
+            _root = _root / safe_path_part
+            
+    # make root directory
+    if not _root.is_dir():
+        _root.mkdir( parents=True )
+
+    # Find valid path
+    valid_path = find_available_path(
+        _root,
+        base_name,
+        file
+    )
+
+    # Optionally create file/dir
+    if create:
+        if file:
+            valid_path.touch()
+        else:
+            valid_path.mkdir()
+
+    return valid_path
         
