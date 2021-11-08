15ba1ec181d83c017b2694fa986eabe8b1a71961
DRS <david.rodriguez.1@etu.unige.ch>
2021-11-08 17:16:30 +0100

    Release v0.3

--- docs/source_base/conf.py
+++ docs/source_base/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, DavidRodriguezSoaresCUI'
 author = 'DavidRodriguezSoaresCUI'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.dev1'
+release = 'v0.3'
 
 
 # -- General configuration ---------------------------------------------------
